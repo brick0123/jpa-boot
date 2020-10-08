c1993c8b04ffda14689dbc11b5913a8d275ca757
한진호 <tlfanfvk@naver.com>
2020-10-08 23:19:21 +0900

    Initial commit

---
+++ src/main/java/jpa/boot/jpaboot/JpaBootApplication.java
@@ -0,0 +1,13 @@
+package jpa.boot.jpaboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBootApplication.class, args);
+	}
+
+}
