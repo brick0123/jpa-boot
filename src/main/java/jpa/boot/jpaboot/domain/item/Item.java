77fa4d4f6731dda201f11df2648f461482950177
한진호 <tlfanfvk@naver.com>
2020-10-14 15:43:39 +0900

    상품 리파지토리 개발

--- src/main/java/jpa/boot/jpaboot/domain/item/Item.java
+++ src/main/java/jpa/boot/jpaboot/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpa.boot.jpaboot.domain.item;
 
 import jpa.boot.jpaboot.domain.Category;
+import jpa.boot.jpaboot.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,24 @@ Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // == 비즈니스 로직 == //
+
+    /**
+     *  stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
