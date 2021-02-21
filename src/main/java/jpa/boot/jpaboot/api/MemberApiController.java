5ff9f340f119dd762159f0599d879f05c2908d54
brick0123 <tlfanfvk@naver.com>
2021-02-21 14:44:28 +0900

    회원 조회 API

--- src/main/java/jpa/boot/jpaboot/api/MemberApiController.java
+++ src/main/java/jpa/boot/jpaboot/api/MemberApiController.java
@@ -1,5 +1,7 @@
 package jpa.boot.jpaboot.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import jpa.boot.jpaboot.domain.Member;
@@ -7,6 +9,7 @@ import jpa.boot.jpaboot.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +21,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberApiController {
 
   private final MemberService memberService;
+
+  @GetMapping("/api/v1/members")
+  public List<Member> membersV1() {
+    return memberService.findMembers();
+  }
+
+  @GetMapping("/api/v2/members")
+  public Result membersV2() {
+    List<MemberDto> collect = memberService.findMembers().stream()
+        .map(m -> new MemberDto(m.getName()))
+        .collect(Collectors.toList());
+
+    return new Result(collect);
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class Result<T> {
+    private T data;
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class MemberDto {
+    private String name;
+  }
+
 
   @PostMapping("/api/v1/members")
   public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
